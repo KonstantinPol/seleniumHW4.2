bc19f3d1ffe15cdd1eb1e523e551e0eccb6f3048
konstantinpolianskii <occa2009@gmail.com>
2018-07-09 11:14:43 -0500

    update 1.1

--- src/test/java/stepdefs/MyStepdefs.java
+++ src/test/java/stepdefs/MyStepdefs.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import pages.HomePage;
 
+import java.util.concurrent.TimeUnit;
+
 public class MyStepdefs {
 
     HomePage homePage;
@@ -18,9 +20,16 @@ public class MyStepdefs {
 
     @Before
     public void init(){
-        System.setProperty("webdriver.chrome.driver","c:\\Docs\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","/Users/konstantinpolianskii/Desktop/drivers/chromedriver");
         driver = new ChromeDriver();
         homePage = new HomePage(driver);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void close(){
+        driver.close();
+        driver.quit();
     }
 
     @Given("^user navigates to the app url$")
@@ -38,14 +47,96 @@ public class MyStepdefs {
         homePage.setLastName(lastName);
     }
 
-    @After
-    public void close(){
-        driver.close();
-        driver.quit();
+
+    @When("^user fills gender$")
+    public void userFillsGender() {
+        homePage.typeGender();
     }
 
-    @Then("^form is submitted$")
-    public void formIsSubmitted() {
-        Assert.assertTrue(false);
+    @When("^user fills date of birth with value \"([^\"]*)\"$")
+    public void userFillsDateOfBirthWithValue(String dob){
+        homePage.typeDOB(dob);
+    }
+
+    @When("^user fills address$")
+    public void userFillsAddress() {
+        homePage.typeAddress("555 Emerson cir Bolingbook. IL 60440");
+    }
+
+    @When("^user fills email with value \"([^\"]*)\"$")
+    public void userFillsEmailWithValue(String email){
+           homePage.typeEmail(email);
+    }
+
+    @When("^user fills password with value \"([^\"]*)\"$")
+    public void userFillsPasswordWithValue(String password) {
+        homePage.typePassword(password);
+    }
+
+    @When("^user fills company$")
+    public void userFillsCompany() {
+      homePage.typeCompany("Home inc");
+    }
+
+    @When("^user fills Role$")
+    public void userFillsRole() {
+         homePage.typeRole();
+    }
+
+    @When("^user fills Job$")
+    public void userFillsJob() {
+         homePage.typeJob();
+    }
+
+    @When("^user fills CheckboxOne$")
+    public void userFillsCheckboxOne() {
+           homePage.typeCheckbox1();
+    }
+
+    @When("^user fills CheckboxTwo$")
+    public void userFillsCheckboxTwo() {
+        homePage.typeCheckbox2();
+    }
+
+    @When("^user fills Comments$")
+    public void userFillsComments() {
+        homePage.typeComments("Very excited to do something new in my life, thank you for this opportunity.");
+    }
+
+    @When("^user submit form$")
+    public void userSubmitForm()  {
+        homePage.typeSubmit();
+    }
+
+    @When("^user check form is submitted$")
+    public void userCheckFormIsSubmitted(){
+        Assert.assertEquals(homePage.typeConfirmSubmitted(), "Successfully submitted!");
+    }
+
+
+    @When("^user reload page$")
+    public void userReloadPage() {
+        driver.get("http://katalon-test.s3.amazonaws.com/demo-aut/dist/html/form.html");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/resources/features/FormSubmit.feature
+++ src/test/resources/features/FormSubmit.feature
@@ -5,21 +5,27 @@ Feature: Form automation
   Scenario Outline: Form filling and submition
     Given user navigates to the app url
     When user fills first name with value "<firstname>"
-    And user fills last name with value "<lastName>"
-#    And user fills date of birth
-#    And user fills gender
-#    And user fills email
-#    And user fills company
-#    And user fills password
-#    And user fills address
-#    And user submit form
-    Then form is submitted
+    When user fills last name with value "<lastName>"
+    When user fills gender
+    When user fills date of birth with value "<01.02.2000>"
+    When user fills address
+    When user fills email with value "<test@gmail.com>"
+    When user fills password with value "<12345>"
+    When user fills company
+    When user fills Role
+    When user fills Job
+    When user fills CheckboxOne
+    When user fills CheckboxTwo
+    When user fills Comments
+    When user submit form
+    When user check form is submitted
+    When user reload page
 
     Examples:
-      | firstname | lastName  |
-      | Nick      | Jackson   |
-      | Sergii    | Opanasiuk |
-      | Benson    | Kamary    |
+      | firstname | lastName  |01.02.2000|test@gmail.com|12345|
+      | Nick      | Jackson   |02.03.2001|test2@gmail.com|qwery1234|
+      | Sergii    | Opanasiuk |03.04.2002|test3@gmail.com|qwery5678|
+      | Benson    | Kamary    |04.05.2003|test4@gmail.com|12345qwert|
 
 
 
